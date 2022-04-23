731095defb40a0456adfe891edc5159344cf734f
will-torres <will.torres.jr@gmail.com>
2022-04-23 02:40:16 +0000

    registration views

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,16 +31,57 @@ def contact(request):
     return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['pwd']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'djangoapp/index.html', context)
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        context['message'] = "Invalid request."
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    print("Log out the user '{}'".format(request.user.username))
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    # If it is a GET request, just render the registration page
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            pass
+        if not user_exist:
+            user = User.objects.create_user(username=username, 
+                                            first_name=first_name, 
+                                            last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return render(request, 'djangoapp/index.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["autos.mybluemix.net"]
+ALLOWED_HOSTS = ["autos.mybluemix.net", "localhost"]
 
 
 # Application definition
