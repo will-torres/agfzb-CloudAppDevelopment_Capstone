faedad9133cdd9cd6fcac39092487e3e0ec95874
will-torres <will.torres.jr@gmail.com>
2022-04-26 23:55:37 +0000

    -

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,10 +20,12 @@ urlpatterns = [
     path(route='login/', view=views.login_request, name='login'),
     # path for logout
     path(route='logout/', view=views.logout_request, name='logout'),
+
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path(route="dealer/<int:dealer_id>/", view=views.get_dealer_details, name="dealer_details"),
     # path for add a review view
+    path(route='review/<int:dealer_id>/', view=views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
