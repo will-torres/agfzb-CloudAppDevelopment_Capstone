51423ea6226158c2281f16400bb41227413a8ed9
will-torres <will.torres.jr@gmail.com>
2022-05-03 13:28:37 +0000

    changes to .restapis

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-    path(route="dealer/<int:dealer_id>/", view=views.get_dealer_details, name="dealer_details"),
+    path(route="dealer/<int:id>/", view=views.get_dealer_details, name="dealer_details"),
     # path for add a review view
     path(route='review/<int:dealer_id>/', view=views.add_review, name='add_review')
 
